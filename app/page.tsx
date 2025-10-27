632e3d497a3dbe04ef65a0c9201b1961b3f1d80f
Leulc21 <leulchanie576@gmail.com>
2025-10-27 21:52:37 +0300

    Fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 import CtaSection from "@/components/call_to_action";
 import Features from "@/components/features";
+import Footer from "@/components/footer";
 import MetricsSection from "@/components/metrics";
 import Navbar from "@/components/nav";
+import SimplePricing from "@/components/pricing";
+import Testimonial from "@/components/testimonials";
+import { FlipWords } from "@/components/ui/flipwords";
 import Link from "next/link";
 
 export default function HeroSection() {
@@ -17,7 +21,17 @@ export default function HeroSection() {
         <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-2/5 aspect-[2/0.5] bg-gradient-to-br from-purple-600 to-violet-400 rounded-full opacity-50 blur-2xl"></div>
         <div className="relative mx-auto pt-32 pb-2 lg:max-w-7xl w-full px-5 sm:px-10 md:px-12 lg:px-5 text-center space-y-10">
           <h1 className="text-gray-900 dark:text-white mx-auto max-w-5xl font-bold text-4xl/tight sm:text-5xl/tight lg:text-6xl/tight xl:text-7xl/tight">
-            We create, we design, we develop Modern tools.
+            <div>
+              We create, we design, we develop{" "}
+              <FlipWords
+                words={["creating", "building", "designing", "crafting"]}
+                duration={3000}
+                className="text-blue-600"
+              >
+                {" "}
+                innovative solutions
+              </FlipWords>
+            </div>
           </h1>
           <p className="text-gray-700 dark:text-gray-300 mx-auto max-w-2xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
@@ -130,6 +144,9 @@ export default function HeroSection() {
       <Features />
 
       <CtaSection />
+      <Testimonial />
+      <SimplePricing />
+      <Footer />
     </>
   );
 }
